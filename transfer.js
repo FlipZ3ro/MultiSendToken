d56d56dcf4ad9bb8ece8dea84f5a0e48ec557805
ΛЯΛPZZ.ΣƬΉ <86567519+arapzz@users.noreply.github.com>
2023-07-17 03:29:55 +0700

    Update transfer.js

--- transfer.js
+++ transfer.js
@@ -1,4 +1,4 @@
-// Bridge to access web3
+// Bridge untuk mengakses web3
 const { ethers } = require("ethers");
 const { RPC, ETHERSCAN } = require("./config");
 const addresses = require("./config").addresses;
@@ -6,7 +6,7 @@ const privateKey = require("./config").keys.main;
 
 // Contract ABI dan alamat
 const tokenABI = require("./tokenABI.json");
-const tokenAddress = "ADDRESS TOKEN"; // Ganti dengan alamat kontrak token yang sesuai
+const tokenAddress = "0x226E0D9fBDE51708fC36Bb4E5d1af9728A285cF4"; // Ganti dengan alamat kontrak token yang sesuai
 
 const provider = new ethers.providers.JsonRpcProvider(RPC);
 const wallet = new ethers.Wallet(privateKey, provider);
@@ -17,19 +17,20 @@ const wallet = new ethers.Wallet(privateKey, provider);
 
     const tokenContract = new ethers.Contract(tokenAddress, tokenABI, wallet);
 
-    const transactions = [];
     const transferAmount = ethers.BigNumber.from("100000000000000000000"); // Ganti dengan jumlah token yang ingin Anda transfer (dalam satuan uint256)
 
-    const iterations = 10; // Tentukan jumlah pengulangan untuk melakukan transaksi berulang
+    const iterations = 1000; // Tentukan jumlah pengulangan untuk melakukan transaksi berulang
+
+    let transferCounter = 0; // Inisialisasi counter untuk nomor urut transfer
 
     for (let j = 0; j < iterations; j++) {
       console.log(`=== Iterasi ${j + 1} ===`);
       
-      for (let i = 0; i < 450; i++) {
+      for (let i = 0; i < 1999; i++) {
         const toAddress = addresses[`W_${i + 2}`];
         const transaction = await tokenContract.transfer(toAddress, transferAmount);
-        transactions.push(transaction);
-        console.log(`Transfer Berhasil ke ${toAddress}. Tx => [${transaction.hash}]`);
+        transferCounter++; // Tambahkan counter setiap kali transfer berhasil
+        console.log(`Transfer Berhasil ke ${toAddress}. Tx => [${transaction.hash}] - Nomor Urut: ${transferCounter}`);
       }
     }
 
